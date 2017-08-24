dee743f7ebd6414b609df3660bd65da54f8608c6
Bryan Whyte <bwhyte21@outlook.com>
2017-08-23 23:01:09 -0400

    Add project files.

---
+++ Squarebuster/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Squarebuster.MvcApplication" Language="C#" %>
---
+++ Squarebuster/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Squarebuster.Startup))]
+namespace Squarebuster
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
