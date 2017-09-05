57f67498b5ee2875ec3213f5b81f0f553a2ff6bd
Bryan Whyte <bwhyte21@outlook.com>
2017-09-05 00:06:10 -0400

    adding new css to site adding mvc fundementals

--- Squarebuster/Controllers/MoviesController.cs
+++ Squarebuster/Controllers/MoviesController.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using Squarebuster.Models;
+using System;
 using System.Web.Mvc;
-using Squarebuster.Models;
 
 namespace Squarebuster.Controllers
 {
@@ -15,6 +12,30 @@ namespace Squarebuster.Controllers
             var movie = new Movie() { Name = "Shrek!" };
 
             return View(movie);
+            //return Content("Hello World!");
+            //return HttpNotFound();
+            //return new EmptyResult();
+            //return RedirectToAction("Index", "Home", new { page = 1, sortBy = "name" });
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return Content("id=" + id);
+        }
+
+        // GET: Movies
+        // ? makes it a nullable int and string is a reference type, making it nullable
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+            {
+                pageIndex = 1;
+            }
+            if (String.IsNullOrWhiteSpace(sortBy))
+            {
+                sortBy = "Name";
+            }
+            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
         }
     }
 }
